af28749d2fd3e5c49c85e3ecfcee4cf73c579aef
Abhimanyu-dev <abhimanyusolanki4841@gmail.com>
2025-08-14 11:12:34 +0530

    Added Navbar hover

---
+++ data/navLinks.ts
@@ -0,0 +1,44 @@
+export const navLinks = [
+	{
+		title: "About IITK",
+		href: "/about",
+		children: [
+			{ title: "About IITK", href: "/about" },
+			{ title: "Academics", href: "/about#academics" },
+			{ title: "Research & Development", href: "/about#research" },
+			{ title: "Alumni", href: "/about#alumni" },
+			{ title: "Faculty", href: "/about#faculty" },
+			{ title: "Activities", href: "/about#activities" },
+		],
+	},
+	{
+		title: "For Students",
+		href: "/students",
+		children: [
+			{ title: "Student Home", href: "/students" },
+			{ title: "Placement Insights", href: "/insights" },
+			{ title: "Procedure", href: "/students#steps" },
+			{ title: "Resources", href: "/students#resources" },
+			{ title: "Preparation Roadmaps", href: "/students#prepRoadmaps" },
+		],
+	},
+	{
+		title: "Recruiter",
+		href: "/companies",
+		children: [
+			{ title: "Companies Home", href: "/companies" },
+			{ title: "Procedure", href: "/companies#steps" },
+			{ title: "Internship Policy", href: "/companies#ipolicy" },
+			{ title: "Facilities", href: "/companies#facilities" },
+			{ title: "Resources", href: "/companies#resources" },
+		],
+	},
+	{
+		title: "Activities",
+		href: "#",
+		children: [
+			{ title: "Samvardhan", href: "/samvardhan" },
+			{ title: "Contact", href: "/placement-coordinators" },
+		],
+	},
+]
